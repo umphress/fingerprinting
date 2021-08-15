60f18ec41ca633f54ecf28927dd2c26af64ce086
umphress <umphress@eng.auburn.edu>
2021-08-15 13:26:53 -0500

    update to puml

--- click/blinkLights.py
+++ click/blinkLights.py
@@ -3,10 +3,15 @@
 #    - This appears to be tied to a specific IP.  It only blinks 192.168.1.32.
 
 import socket
-BEACON_PORT = 2771
+BEACON_PORT = 2770  # This is a "veronica" port - 
 CLICK_PORT = 25425
 BEACON_MESSAGE = "4b4f50000100d2090e0045016643c0a8012000d07c121589"  # on 192.168.1.32, not all
-# BEACON_MESSAGE 4b4f50000100edd5040045016680"
+BEACON_MESSAGE = "4b4f500001004d340e0045016643c0a8011d00d07c19033e"
+#  Dissection of BEACON_MESSAGE
+#          4b 4f 50 00 01 00    (this part is common with service discovery request)
+#        + 4d 34 0e 00 45 01 66 43
+#        + IP address (c0a8011d in this case)
+#        + MAC address (00:d0:7c:19:03:3e in this case)
 
 beacon = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 beacon.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
--- click/serveAndReceiveParse.py
+++ click/serveAndReceiveParse.py
@@ -3,6 +3,17 @@ BEACON_PORT = 2770
 CLICK_PORT = 25425
 BEACON_MESSAGE = "4b4f50000100edd5040045016680"
 # BEACON_MESSAGE 4b 4f 50 00 01 00 ed d5 04 00 45 01 66 80"
+# Starting Offset    Ending Offset    Length    Content
+# 0    17    18    unknown
+# 18    21    4    PLC IP
+# 22    25    4    Subnet Mask
+# 26    29    4    Default IP
+# 30    35    6    MAC
+# 36    59    24    PLC name
+# 60    66    7    unknown
+# 67    68    2    Firmware version
+# 69    70    2    PLC Status Flags
+# 71    72    2    unknown
 
 beacon = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 beacon.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -27,7 +38,19 @@ while True:
         # print("--client received data: %s" % data.hex())
         print(f'--client received data (hex):', data.hex())
         print(f'--client received data (raw): {data}')
-        print()
+        parse = []
+        parse.append(data.hex()[0:17])
+        parse.append(data.hex()[17:21])
+        parse.append(data.hex()[21:25])
+        parse.append(data.hex()[25:29])
+        parse.append(data.hex()[29:35])
+        parse.append(data[36:59])
+        parse.append(data.hex()[59:66])
+        parse.append(data.hex()[66:68])
+        parse.append(data.hex()[68:70])
+        parse.append(data.hex()[70:72])        
+        
+        print(parse)
     except:
         break
 print(responseCount, " responses ... done")
